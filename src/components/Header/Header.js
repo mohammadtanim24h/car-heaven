a3d8f2b0ea18b4e63068f72e1b88eee34790b8ee
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-03-25 21:46:55 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='title'>Car Heaven</h1>
+            <h2 className='title2'>Choose Cars</h2>
+        </div>
+    );
+};
+
+export default Header;
