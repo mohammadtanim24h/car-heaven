93720f3cbb66f90260dce64e351bceec6290ed75
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-03-26 17:19:41 +0600

    questions and answers added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Cars from "./components/Cars/Cars";
 import Header from "./components/Header/Header";
+import Questions from "./components/Questions/Questions";
 
 function App() {
     return (
         <div className="App container-fluid">
             <Header></Header>
             <Cars></Cars>
+            <Questions></Questions>
         </div>
     );
 }
---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Questions.css';
+
+const Questions = () => {
+    return (
+        <div className='quesion-container container mt-5'>
+            <div className='question'>
+                <h3>How React Works</h3>
+                <p>React keeps a copy of the Real DOM called Virtual DOM. It is represented as a tree. When the state of a component changes, a new Virtual DOM tree is created. So then react has two representations of the Virtual DOM. One before the change of the state and one after the change of the state. With the help of the diff algorithm, it compares both DOMs and decides which things have been changed. Then it efficiently updates the Real DOM in the best possible way.</p>
+            </div>
+            <div className='question'>
+                <h3>Props Vs State</h3>
+                <p>States contain data or information that can change depending on our interaction in a website. Props are used to send data from one component to another component. Props are read-only and immutable which means we cannot change it. On the other hand State is mutable and it can change. State is handled inside a component and props are handled outside of a component</p>
+            </div>
+        </div>
+    );
+};
+
+export default Questions;
